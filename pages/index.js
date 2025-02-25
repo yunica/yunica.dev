e66e94efbd373da17a1b58a5523d402517541540
Junior flores <fmjg21@gmail.com>
2025-02-25 12:57:38 -0500

    update page index

--- pages/index.js
+++ pages/index.js
@@ -1,170 +1,271 @@
+import { useState, useEffect } from "react";
 // @mui material components
-import Image from "next/image";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import Card from "@mui/material/Card";
+import MDButton from "/components/MDButton";
+
+// pages/api/markdown.js
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+// Custom components
 import MDBox from "/components/MDBox";
-import MuiLink from "@mui/material/Link";
+import MDTypography from "/components/MDTypography";
 
 // Custom examples
 import DashboardLayout from "/examples/LayoutContainers/DashboardLayout";
-import DefaultInfoCard from "/examples/Cards/InfoCards/DefaultInfoCard";
-// Images
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Globe from "/pagesComponents/globe";
-import Icon from "@mui/material/Icon";
 
-import profileBw from "/assets/images/profile/profile_main_bw-min.png";
-import profileColor from "/assets/images/profile/profile_main_color-min.png";
-import { Card } from "@mui/material";
-import MDTypography from "/components/MDTypography";
+// Overview page components
+import Header from "/pagesComponents/pages/profile/components/Header";
+import TimelineList from "/examples/Timeline/TimelineList";
+import TimelineItem from "/examples/Timeline/TimelineItem";
+
+// Data
+import { experience, expertice, contribution } from "/utils/constants";
 import { useMaterialUIController } from "/context";
 
-function Main() {
-  const [controller] = useMaterialUIController();
+// Images
+import SocialContact from "/pagesComponents/social/contact";
+import ListSkill from "/pagesComponents/social/skills";
 
+export async function getStaticProps() {
+  const markdownDirectory = path.join(process.cwd(), "public", "markdown");
+  const filenames = fs.readdirSync(markdownDirectory);
+
+  const dataPromises = filenames.map(async (filename) => {
+    const filePath = path.join(markdownDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data, content } = matter(fileContents);
+
+    return {
+      filename,
+      ...data,
+    };
+  });
+  const data = await Promise.all(dataPromises);
+
+  // data.sort((a, b) => (a.date > b.date ? -1 : 1));
+  return { props: { data } };
+}
+
+const Main = ({ data }) => {
+  const [controller] = useMaterialUIController();
   const { darkMode } = controller;
 
+  const renderTimelineItems = experience.map(
+    ({ color, icon, title, dateTime, description, details }, k) => (
+      <TimelineItem
+        key={title + color}
+        icon={icon}
+        title={title}
+        dateTime={dateTime}
+        description={description}
+        details={details}
+        lastItem={k === experience.length - 1}
+      />
+    )
+  );
+
+  const slugs = [...new Set([].concat(...data.map((i) => i.category)))];
+  const renderTechSkill = slugs.map((i) => (
+    <MDButton
+      key={i}
+      size="small"
+      color={darkMode ? "white" : "dark"}
+      circular
+      variant="outlined"
+      sx={{ margin: "4px" }}
+    >
+      {i.replace("dot", ".").replace("amazon", "amazon ")}
+    </MDButton>
+  ));
+
+  const years = new Date().getFullYear() - 2021;
+
+  const renderAbout = () => (
+    <>
+      <MDBox
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        pt={2}
+        px={2}
+      >
+        <MDTypography
+          variant="h2"
+          sx={{ fontSize: "1rem" }}
+          fontWeight="medium"
+          textTransform="uppercase"
+        >
+          <MDTypography variant="span" fontWeight="bold" opacity={0.7}>
+            01{" "}
+          </MDTypography>
+          About me
+        </MDTypography>
+      </MDBox>
+      <MDBox p={2}>
+        <MDBox>
+          <MDTypography variant="body2" color={darkMode ? "white" : "dark"}>
+            Data Engineer specialized in{" "}
+            <b>GIS and geospatial data processing</b> with over{" "}
+            <b>{years} years of experience</b> in designing, developing, and
+            optimizing <b>real-time data pipelines</b>, satellite image
+            processing, and automating ETL workflows. With experience in{" "}
+            <b>
+              Big Data, Machine Learning, cloud services (AWS/GCP), and
+              geospatial application development
+            </b>
+            . Recognized for implementing custom algorithms for geospatial
+            analysis and developing high-impact platforms used by government
+            institutions and international organizations.
+          </MDTypography>
+        </MDBox>
+      </MDBox>
+    </>
+  );
+  const renderExpertise = () => (
+    <>
+      <MDBox
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        pt={2}
+        px={2}
+      >
+        <MDTypography
+          variant="h2"
+          sx={{ fontSize: "1rem" }}
+          fontWeight="medium"
+          textTransform="uppercase"
+        >
+          <MDTypography variant="span" fontWeight="bold" opacity={0.7}>
+            02{" "}
+          </MDTypography>
+          my expertise
+        </MDTypography>
+      </MDBox>
+      <MDBox p={2}>
+        <MDBox lineHeight={1}>
+          <ListSkill data={expertice} />
+        </MDBox>
+      </MDBox>
+    </>
+  );
+  const renderTechskills = () => (
+    <>
+      <MDBox
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        pt={2}
+        px={2}
+      >
+        <MDTypography
+          variant="h2"
+          sx={{ fontSize: "1rem" }}
+          fontWeight="medium"
+          textTransform="uppercase"
+        >
+          <MDTypography variant="span" fontWeight="bold" opacity={0.7}>
+            03{" "}
+          </MDTypography>{" "}
+          Tech skills
+        </MDTypography>
+      </MDBox>
+      <MDBox p={2}>
+        <MDBox>{renderTechSkill}</MDBox>
+      </MDBox>
+    </>
+  );
+  const renderHighlightedProjects = () => (
+    <>
+      <MDBox
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        pt={2}
+        px={2}
+      >
+        <MDTypography
+          variant="h2"
+          sx={{ fontSize: "1rem" }}
+          fontWeight="medium"
+          textTransform="uppercase"
+        >
+          <MDTypography variant="span" fontWeight="bold" opacity={0.7}>
+            04{" "}
+          </MDTypography>
+          Highlighted Projects
+        </MDTypography>
+      </MDBox>
+      <MDBox p={2}>
+        <MDBox lineHeight={1}>
+          <ListSkill data={contribution} />
+        </MDBox>
+      </MDBox>
+    </>
+  );
+  const renderMyCarrer = () => (
+    <TimelineList title="MY CAREER" index="05" dark={darkMode} shadow={false}>
+      {renderTimelineItems}
+    </TimelineList>
+  );
+  const renderGetInTouch = () => (
+    <>
+      <MDBox
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        pt={2}
+        px={2}
+      >
+        <MDTypography
+          variant="h2"
+          sx={{ fontSize: "1rem" }}
+          fontWeight="medium"
+          textTransform="uppercase"
+        >
+          <MDTypography variant="span" fontWeight="bold" opacity={0.7}>
+            06{" "}
+          </MDTypography>{" "}
+          get in touch
+        </MDTypography>
+      </MDBox>
+      <MDBox p={2}>
+        <SocialContact />
+      </MDBox>
+    </>
+  );
   return (
     <DashboardLayout>
-      <Grid maxWidth="100%" width="100%">
-        <MDBox
-          minWidth="700px"
-          height="95%"
-          position="absolute"
-          right="5px"
-          top="5%"
-          display={{ sm: "none", md: "block" }}
-        >
-          <Globe width={800} height={800} />
-        </MDBox>
-        <MDBox direction="column" zIndex="0">
+      <Header>
+        <MDBox mt={2} mb={1}>
           <Grid container spacing={2}>
-            <Grid
-              item
-              xs={12}
-              md={8}
-              xl={8}
-              style={{ position: "relative", minHeight: "100vh" }}
-            >
-              <div
-                style={{
-                  position: "relative",
-                  width: "auto",
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "flex-end",
-                }}
-              >
-                <Box
-                  sx={{
-                    display: { xs: "none", md: "block" },
-                    height: "90%",
-                    minWidth: "40%",
-                    maxWidth: "70%",
-                  }}
-                >
-                  <Image
-                    src={profileColor}
-                    alt="Junior Flores - Geospatial Data Engineer"
-                    layout="fill"
-                    objectFit="cover"
-                    hidden={true}
-                    quality={70}
-                    objectPosition="top"
-                  />
-                </Box>
-                <Grid container spacing={1} zIndex={1} mt={5}>
-                  <Grid item md={12} mt={3}>
-                    <MDTypography
-                      variant="h1"
-                      fontWeight="bold"
-                      textTransform="uppercase"
-                      textGradient={true}
-                      color={darkMode ? "primary" : "primary"}
-                    >
-                      Junior Flores
-                    </MDTypography>
-                  </Grid>
-
-                  <Grid item md={12}>
-                    <MDTypography
-                      variant="h3"
-                      fontWeight="regular"
-                      textTransform="uppercase"
-                      textGradient={darkMode}
-                      color={darkMode ? "primary" : "primary"}
-                    >
-                      Geospatial data engineer
-                    </MDTypography>
-                  </Grid>
-                  <Grid item md={12}>
-                    <Card>
-                      <MDBox p={2}>
-                        <MDTypography
-                          variant="body2"
-                          color="dark"
-                          fontWeight="light"
-                        >
-                          I am a Data Engineer specialized in Geospatial
-                          Information Systems(GIS), with extensive experience in
-                          developing optimized ETL (Extract, Transform, Load)
-                          processes and in creating tools for generating data
-                          for Machine Learning. As a Data Engineer, I am
-                          committed to refining data pipelines, effectively
-                          transforming raw data into essential resources for
-                          various applications.
-                          <br />I have a strong proficiency in backend
-                          development, which I consider my area of expertise.
-                        </MDTypography>
-                      </MDBox>
-                    </Card>
-                  </Grid>
-                  <Grid item xs={6} md={3} lg={3}>
-                    <DefaultInfoCard
-                      icon="insights"
-                      title="+5"
-                      description="Years of experience "
-                    />
-                  </Grid>
-                  <Grid item xs={6} md={3} lg={3}>
-                    <DefaultInfoCard
-                      icon="work"
-                      title="+26"
-                      description="Projects completed"
-                    />
-                  </Grid>
-                  <Grid item xs={6} md={3} lg={3}>
-                    <DefaultInfoCard
-                      icon="terminal"
-                      title="+3"
-                      description="Open source packages"
-                    />
-                  </Grid>
-                  <Grid item xs={6} md={3} lg={3}>
-                    <DefaultInfoCard
-                      color="success"
-                      iconLink={
-                        <MuiLink
-                          href="/junior_flores_cv.pdf"
-                          target="_blank"
-                          rel="noreferrer"
-                          download
-                        >
-                          <Icon fontSize="large">description</Icon>
-                        </MuiLink>
-                      }
-                      title="1"
-                      description="Download Resume"
-                    />
-                  </Grid>
-                </Grid>
-              </div>
+            <Grid item xs={12} md={6} xl={4}>
+              <Card sx={{ height: "100%", boxShadow: "none" }}>
+                {renderAbout()}
+                {renderExpertise()}
+                {renderTechskills()}
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
+              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+              <Card sx={{ height: "100%", boxShadow: "none" }}>
+                {renderHighlightedProjects()}
+              </Card>
+              <Divider orientation="vertical" sx={{ mx: 0 }} />
+            </Grid>
+            <Grid item xs={12} xl={4}>
+              {renderMyCarrer()}
+              {renderGetInTouch()}
             </Grid>
           </Grid>
         </MDBox>
-      </Grid>
+      </Header>
     </DashboardLayout>
   );
-}
+};
 
 export default Main;
--- pages/services.js
+++ pages/services.js
@@ -1,12 +1,168 @@
+// @mui material components
+import Image from "next/image";
+import MDBox from "/components/MDBox";
+import MuiLink from "@mui/material/Link";
+
 // Custom examples
 import DashboardLayout from "/examples/LayoutContainers/DashboardLayout";
-
+import DefaultInfoCard from "/examples/Cards/InfoCards/DefaultInfoCard";
 // Images
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Globe from "/pagesComponents/globe";
+import Icon from "@mui/material/Icon";
+
+import profileBw from "/assets/images/profile/profile_main_bw-min.png";
+import profileColor from "/assets/images/profile/profile_main_color-min.png";
+import { Card } from "@mui/material";
+import MDTypography from "/components/MDTypography";
+import { useMaterialUIController } from "/context";
 
 function Services() {
+  const [controller] = useMaterialUIController();
+
+  const { darkMode } = controller;
+
   return (
     <DashboardLayout>
-      <h1>Services</h1>
+      <Grid maxWidth="100%" width="100%">
+        <MDBox
+          minWidth="700px"
+          height="95%"
+          position="absolute"
+          right="5px"
+          top="5%"
+          display={{ sm: "none", md: "block" }}
+        >
+          <Globe width={800} height={800} />
+        </MDBox>
+        <MDBox direction="column" zIndex="0">
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              md={8}
+              xl={8}
+              style={{ position: "relative", minHeight: "100vh" }}
+            >
+              <div
+                style={{
+                  position: "relative",
+                  width: "auto",
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: { xs: "none", md: "block" },
+                    height: "90%",
+                    minWidth: "40%",
+                    maxWidth: "70%",
+                  }}
+                >
+                  <Image
+                    src={profileColor}
+                    alt="Junior Flores - Geospatial Data Engineer"
+                    layout="fill"
+                    objectFit="cover"
+                    hidden={true}
+                    quality={70}
+                    objectPosition="top"
+                  />
+                </Box>
+                <Grid container spacing={1} zIndex={1} mt={5}>
+                  <Grid item md={12} mt={3}>
+                    <MDTypography
+                      variant="h1"
+                      fontWeight="bold"
+                      textTransform="uppercase"
+                      textGradient={true}
+                      color={darkMode ? "primary" : "primary"}
+                    >
+                      Junior Flores
+                    </MDTypography>
+                  </Grid>
+
+                  <Grid item md={12}>
+                    <MDTypography
+                      variant="h3"
+                      fontWeight="regular"
+                      textTransform="uppercase"
+                      textGradient={darkMode}
+                      color={darkMode ? "primary" : "primary"}
+                    >
+                      Geospatial data engineer
+                    </MDTypography>
+                  </Grid>
+                  <Grid item md={12}>
+                    <Card>
+                      <MDBox p={2}>
+                        <MDTypography
+                          variant="body2"
+                          color="dark"
+                          fontWeight="light"
+                        >
+                          I am a Data Engineer specialized in Geospatial
+                          Information Systems(GIS), with extensive experience in
+                          developing optimized ETL (Extract, Transform, Load)
+                          processes and in creating tools for generating data
+                          for Machine Learning. As a Data Engineer, I am
+                          committed to refining data pipelines, effectively
+                          transforming raw data into essential resources for
+                          various applications.
+                          <br />I have a strong proficiency in backend
+                          development, which I consider my area of expertise.
+                        </MDTypography>
+                      </MDBox>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={6} md={3} lg={3}>
+                    <DefaultInfoCard
+                      icon="insights"
+                      title="+5"
+                      description="Years of experience "
+                    />
+                  </Grid>
+                  <Grid item xs={6} md={3} lg={3}>
+                    <DefaultInfoCard
+                      icon="work"
+                      title="+26"
+                      description="Projects completed"
+                    />
+                  </Grid>
+                  <Grid item xs={6} md={3} lg={3}>
+                    <DefaultInfoCard
+                      icon="terminal"
+                      title="+3"
+                      description="Open source packages"
+                    />
+                  </Grid>
+                  <Grid item xs={6} md={3} lg={3}>
+                    <DefaultInfoCard
+                      color="success"
+                      iconLink={
+                        <MuiLink
+                          href="/junior_flores_cv.pdf"
+                          target="_blank"
+                          rel="noreferrer"
+                          download
+                        >
+                          <Icon fontSize="large">description</Icon>
+                        </MuiLink>
+                      }
+                      title="1"
+                      description="Download Resume"
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+            </Grid>
+          </Grid>
+        </MDBox>
+      </Grid>
     </DashboardLayout>
   );
 }
